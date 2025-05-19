f9f7c1d9ee42adca4cd63ec1a2c7ab1e88bba5ea
JulioLeiteSilva <julioleite93d@gmail.com>
2025-05-19 16:09:53 -0300

    feat: create categories create service

---
+++ finbuddy/src/services/Categories/index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { httpsCallable } from "firebase/functions";
+import { CategoryRequestDTOSchemaType } from "../../schemas/categories";
+import { functions } from "../firebase";
+import { useCategoriesStore } from "../../store/categoriesStore";
+import { useSnackbarStore } from "../../store/useSnackbarStore";
+import { getFirebaseAuthErrorMessage } from "../../utils/firebaseErrorMenssages";
+
+export const CreateCategory = async (
+  data: CategoryRequestDTOSchemaType
+) => {
+  const { showSnackbar } = useSnackbarStore.getState();
+
+  try {
+    const createCategoryFn = httpsCallable(functions, 'category-createCategory');
+    const response = await createCategoryFn(data);
+    console.log('Categoria criada:', response.data);
+
+    const { fetchCategories } = useCategoriesStore.getState();
+    await fetchCategories();
+
+    showSnackbar('Categoria criada com sucesso!', 'success');
+  } catch (error) {
+    const errorCode = (error as any)?.code || 'unknown';
+    const message = getFirebaseAuthErrorMessage(errorCode);
+    showSnackbar(message, 'error');
+    console.error('Erro ao criar categoria:', error);
+  }
+};
