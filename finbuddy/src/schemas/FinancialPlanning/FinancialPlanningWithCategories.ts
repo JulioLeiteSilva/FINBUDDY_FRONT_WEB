2fcfeacf1e00f863a66b39b74042bd602a94f3ef
julio leite <julioleite93d@gmail.com>
2025-10-05 13:09:35 -0300

    feat: add base schamas

---
+++ finbuddy/src/schemas/FinancialPlanning/FinancialPlanningWithCategories.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+import { CategorySchema } from "../Categories";
+import { FinancialPlanningSchema } from "./FinancialPlanning";
+
+const categoryAllocationSchema = z.object({
+  category: CategorySchema,
+  value: z.number(),
+  spent: z.number().optional(),
+  paidSpent: z.number().optional(),
+  unpaidSpent: z.number().optional(),
+  remaining: z.number().optional(),
+  percentUsed: z.number().optional(),
+  percentPaidUsed: z.number().optional(),
+  percentUnpaidUsed: z.number().optional(),
+});
+
+export const FinancialPlanningWithCategoriesSchema =
+  FinancialPlanningSchema.omit({ categoryAllocations: true }).extend({
+    categoryAllocations: z.array(categoryAllocationSchema),
+    totalSpent: z.number().optional(),
+    totalPaidSpent: z.number().optional(),
+    totalUnpaidSpent: z.number().optional(),
+    remainingBudget: z.number().optional(),
+    percentUsed: z.number().optional(),
+    percentPaidUsed: z.number().optional(),
+    percentUnpaidUsed: z.number().optional(),
+    unallocatedAmount: z.number().optional(),
+  });
+
+export type FinancialPlanningWithCategoriesType = z.infer<
+  typeof FinancialPlanningWithCategoriesSchema
+>;
